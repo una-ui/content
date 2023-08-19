e9b7a9974d6c34f06c694a594ed8dc4e2fca1920
phojie rengel <phojrengel@gmail.com>
2023-08-19 08:18:32 +0800

    chore: init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,8 @@
 module.exports = {
   root: true,
-  extends: ['@nuxt/eslint-config'],
+  extends: ['@antfu', '@unocss'],
   rules: {
-    // Global
-    semi: ['error', 'never'],
-    quotes: ['error', 'single'],
-    'quote-props': ['error', 'as-needed'],
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-
-    // Vue
-    indent: ['error', 2],
     'vue/multi-word-component-names': 0,
-    'vue/no-v-html': 0
-  }
+    'vue/no-v-html': 0,
+  },
 }
