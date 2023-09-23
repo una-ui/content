5d3c88cd0412c833bff0a5ac96b9e95e285f9327
phojie rengel <phojrengel@gmail.com>
2023-09-23 18:17:04 +0800

    feat: add color-mode component

---
+++ components/ColorMode.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+function toggleDark() {
+  colorMode.preference = colorMode.preference === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <NButton
+    btn="ghost-gray square"
+    size="md"
+    aria-label="Toggle dark mode"
+    @click="toggleDark()"
+  >
+    <NIcon
+      name="i-heroicons-sun-20-solid dark:i-heroicons-moon-20-solid"
+    />
+  </NButton>
+</template>
