ddb47734a7999a9466d2886fad139b958ed4682d
phojie rengel <phojrengel@gmail.com>
2023-09-24 01:39:43 +0800

    feat: add misc components

--- components/ColorMode.vue
+++ components/ColorMode.vue
@@ -7,13 +7,12 @@ function toggleDark() {
 
 <template>
   <NButton
-    btn="ghost-gray square"
-    size="md"
+    btn="text-gray square"
     aria-label="Toggle dark mode"
+    icon
+    label="i-heroicons-sun-20-solid dark:i-heroicons-moon-20-solid"
+    class="opacity-70 hover:opacity-100"
+    size="13px"
     @click="toggleDark()"
-  >
-    <NIcon
-      name="i-heroicons-sun-20-solid dark:i-heroicons-moon-20-solid"
-    />
-  </NButton>
+  />
 </template>
--- components/Ellipsis.vue
+++ components/Ellipsis.vue
@@ -15,7 +15,7 @@ const props = withDefaults(defineProps<EllipsisProps>(), {
   top: '0',
   left: '0',
   blur: '300',
-  opacity: '100',
+  opacity: '1',
   radius: '50%',
 })
 </script>
@@ -33,6 +33,7 @@ const props = withDefaults(defineProps<EllipsisProps>(), {
     }"
   >
     <div
+      v-bind="$attrs"
       class="h-full w-full from-primary to-primary-200 bg-gradient-to-r"
       :style="{
         borderRadius: props.radius,
