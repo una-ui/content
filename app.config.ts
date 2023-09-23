2d2bafd38e0b7490df5a4d6bf88d3785bcaa3e3e
phojie rengel <phojrengel@gmail.com>
2023-09-24 01:40:09 +0800

    chore: add app config

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,29 @@
+import type { contentConfig } from './types'
+
+// @unocss-include
 export default defineAppConfig({
   una: {
-    content: {
-      title: 'Una UI',
+    content: <contentConfig> {
+      title: 'Una UI (Content)',
+      description: 'Craft visually appealing content effortlessly using Una UI',
+      ogImage: '',
+      url: 'https://unaui.com',
+      socials: {
+        twitter: {
+          url: 'https://twitter.com/una_ui',
+          icon: 'i-carbon-logo-twitter',
+        },
+        github: {
+          url: 'https://github.com/una-ui/una-ui',
+          icon: 'i-carbon-logo-github',
+        },
+      },
+      github: {
+        root: 'content',
+        edit: true,
+        contributors: false,
+      },
+      layout: 'default',
     },
   },
 })
---
+++ types.ts
@@ -0,0 +1,18 @@
+export interface contentConfig {
+  title?: string
+  description?: string
+  ogImage?: string
+  url?: string
+  socials?: {
+    [key: string]: {
+      url: string
+      icon: string
+    }
+  }
+  github?: {
+    root?: string
+    edit?: boolean
+    contributors?: boolean
+  }
+  layout?: string
+}
