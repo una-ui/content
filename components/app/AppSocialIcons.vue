e4d4ea4d03ec80aac7acfe32d1a4b73943666fa7
phojie rengel <phojrengel@gmail.com>
2023-09-24 01:16:57 +0800

    feat: add social-icons component

---
+++ components/app/AppSocialIcons.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { socials } = useAppConfig().una.content
+</script>
+
+<template>
+  <div class="gap-x-3">
+    <NButton
+      v-for="(social, i) in socials"
+      :key="i"
+      icon
+      :to="social.url"
+      target="_blank"
+      size="14px"
+      class="opacity-70 hover:opacity-100"
+      btn="text-gray square"
+      :label="social.icon"
+      :aria-label="i"
+    />
+  </div>
+</template>
