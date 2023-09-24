f3fc93067cba2e95952c955d00526110fe4dd6ca
phojie rengel <phojrengel@gmail.com>
2023-09-24 17:15:17 +0800

    style: fix una variable config

--- app.vue
+++ app.vue
@@ -5,7 +5,9 @@ useSetupPageHeader()
 
 <template>
   <div>
-    <NuxtLoadingIndicator color="repeating-linear-gradient(to right,var(--c-primary) 0%,var(--c-primary-active) 100%)" />
+    <NuxtLoadingIndicator
+      color="repeating-linear-gradient(to right,rgb(var(--una-primary)) 0%,rgb(var(--una-primary-active)) 100%)"
+    />
     <AppLayout>
       <NuxtPage />
     </AppLayout>
