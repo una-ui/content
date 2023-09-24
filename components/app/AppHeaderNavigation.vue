7cadea4c21c71ad84ad0ca90b6505315c60c1821
phojie rengel <phojrengel@gmail.com>
2023-09-24 16:35:34 +0800

    chore(component): init

--- components/app/AppHeaderNavigation.vue
+++ components/app/AppHeaderNavigation.vue
@@ -4,17 +4,25 @@ const { navigation } = useContent()
 
 <template>
   <div class="hidden gap-x-4 lg:flex">
-    <NButton
+    <template
       v-for="item in navigation"
       :key="item._id"
-      :to="item._path"
-      :label="item.title"
-      btn="text-gray square hover:text-primary"
-      active-class="text-primary"
-      size="sm"
-      :una="{
-        btn: 'font-normal',
-      }"
-    />
+    >
+      <NButton
+        :to="item._path"
+        :label="item.title"
+        btn="text-gray square hover:text-primary"
+        active-class="text-primary"
+        size="sm"
+        :una="{
+          btn: 'font-normal',
+        }"
+      />
+
+      <!--
+        TODO: add dropdown to item children
+        :trailing="item.children ? 'i-heroicons-chevron-down-20-solid text-sm' : ''"
+       -->
+    </template>
   </div>
 </template>
