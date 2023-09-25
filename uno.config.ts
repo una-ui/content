c38c112bec8a6a3077396db38015ed9fcd81c00a
phojie rengel <phojrengel@gmail.com>
2023-09-26 06:05:22 +0800

    chore: config typography preset

--- uno.config.ts
+++ uno.config.ts
@@ -18,16 +18,99 @@ export default {
 
       // docs components
       'content-docs-page-layout': 'grid grid-cols-5 min-h-screen py-8 lg:gap-x-8',
-      'content-docs-toc': 'border-l border-base py-2',
+      'content-docs-toc': 'border-l border-base pt-2 pb-8',
       'content-docs-header': 'text-sm text-muted mb-2 pl-4',
-      'content-docs-toc-links': 'text-sm font-light text-$c-gray-500',
+      'content-docs-toc-links': 'text-sm text-$c-gray-500',
     },
   ],
-
+  content: {
+    pipeline: {
+      include: [
+        /\.(vue|svelte|[jt]sx|md|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // include js/ts files
+        'src/**/*.{js,ts}',
+      ],
+      // exclude files
+      exclude: ['/.playground/**/*'],
+    },
+  },
   presets: [
     presetUno(),
     presetAttributify(),
-    presetTypography(),
+    presetTypography({
+      cssExtend: {
+        'a': {
+          'text-underline-offset': '5px',
+          'text-decoration-style': 'dashed',
+          'font-weight': '400',
+          'text-decoration-thickness': '1px',
+        },
+        'a:hover': {
+          'text-decoration': 'underline',
+          'text-underline-offset': '5px',
+        },
+        'h2 a:hover': {
+          'text-decoration': 'none',
+        },
+        'p,ul,ol,pre': {
+          'line-height': 1.5,
+        },
+        'h1, h2, h3, h4, h5, h6': {
+          'scroll-margin-top': '100px',
+          'color': 'var(--c-gray-900)',
+          'font-weight': '700',
+        },
+        ':not(pre) > code::before,:not(pre) > code::after': {
+          content: '',
+        },
+        'pre,code': {
+          'background': 'var(--c-gray-50)',
+          'padding': '0.1rem 0.5rem',
+          'color': 'var(--c-brand)',
+          'border-radius': '0.25rem',
+          'font-style': 'normal',
+          'font-weight': '400',
+          'font-size': '16px',
+        },
+        'h1 a, h2 a, h3 a, h4 a, h5 a, h6 a': {
+          'text-decoration': 'none',
+          'color': 'inherit',
+          'font-weight': '600',
+        },
+        'h1': {
+          'margin': '1rem 0',
+          'font-size': '2.75em',
+          'color': 'var(--c-brand)',
+        },
+        'h2': {
+          'margin': '1em 0 1em',
+          'font-size': '2.25em',
+        },
+        'h3': {
+          'margin': ' 0 .5em',
+          'font-size': '1.75em',
+        },
+        'h4': {
+          'margin': '0',
+          'font-size': '1.375em',
+        },
+        'h5': {
+          'margin': '0',
+          'font-size': '1.25em',
+        },
+        'h6': {
+          'margin': '1em 0',
+          'font-size': '1.125em',
+        },
+        'hr': {
+          'border-top': '.5px solid var(--un-prose-hr)',
+        },
+        'blockquote': {
+          'color': 'var(--c-gray-500)',
+          'font-style': 'normal',
+        },
+      },
+    }),
     presetIcons({
       scale: 1.2,
       extraProperties: {
