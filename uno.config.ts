1b8601a85122fd971d437ad9a828348b9889e202
phojie rengel <phojrengel@gmail.com>
2023-09-26 01:40:55 +0800

    chore: init

--- uno.config.ts
+++ uno.config.ts
@@ -12,7 +12,15 @@ import presetUna from '@una-ui/preset'
 export default {
   shortcuts: [
     {
-      'n-container': 'container lg:px-8 mx-auto px-4 sm:px-6',
+      // apps components
+      'content-container': 'container lg:px-8 mx-auto px-4 sm:px-6',
+      'content-header': 'sticky top-0 z-50 bg-white/50 backdrop-blur-md dark:bg-gray-950/50 border-base',
+
+      // docs components
+      'content-docs-page-layout': 'grid grid-cols-5 min-h-screen py-8 lg:gap-x-8',
+      'content-docs-toc': 'border-l border-base py-2',
+      'content-docs-header': 'text-sm text-muted mb-2 pl-4',
+      'content-docs-toc-links': 'text-sm font-light text-$c-gray-500',
     },
   ],
 
