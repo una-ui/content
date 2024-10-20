566ba52dc8ee483c7e5d523530159f49e5592616
Phojie Rengel <phojrengel@gmail.com>
2024-10-21 06:45:20 +0800

    chore: init

--- app.config.ts
+++ app.config.ts
@@ -19,27 +19,32 @@ export default defineAppConfig({
         }],
       }, {
         title: 'Credits',
-        links: [{
-          title: 'shadcn-ui',
-          to: 'https://ui.shadcn.com/',
-          description: 'For the beautiful component design & docs design',
-          target: '_blank',
-        }, {
-          title: 'shadcn-vue',
-          to: 'https://www.shadcn-vue.com/',
-          description: 'For the vue port of shadcn-ui & some docs component source',
-          target: '_blank',
-        }, {
-          title: 'Docus',
-          to: 'https://docus.dev/',
-          description: 'For inspiration & some docs component source',
-          target: '_blank',
-        }, {
-          title: 'Nuxt Content',
-          to: 'https://content.nuxt.com/',
-          description: 'Content made easy for Vue Developers',
-          target: '_blank',
-        }],
+        links: [
+          {
+            title: 'Una UI',
+            to: 'https://unaui.com/',
+            description: 'For the beautiful component design',
+            target: '_blank',
+          },
+          {
+            title: 'shadcn-ui',
+            to: 'https://ui.shadcn.com/',
+            description: 'For the beautiful component design & docs design',
+            target: '_blank',
+          },
+          {
+            title: 'shadcn-vue',
+            to: 'https://www.shadcn-vue.com/',
+            description: 'For the vue port of shadcn-ui & some docs component source',
+            target: '_blank',
+          },
+          {
+            title: 'Nuxt Content',
+            to: 'https://content.nuxt.com/',
+            description: 'Content made easy for Vue Developers',
+            target: '_blank',
+          },
+        ],
       }, {
         title: 'Use This Template',
         to: '/getting-started/installation',
