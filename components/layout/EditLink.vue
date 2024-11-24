12632271ea4e727495ceaa82cede50933e04e474
Phojie Rengel <phojrengel@gmail.com>
2024-11-25 07:05:39 +0800

    feat: add edit link functionality and footer component

---
+++ components/layout/EditLink.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { url, enabledDocsFooter, text, icon } = useEditLink()
+</script>
+
+<template>
+  <div v-if="enabledDocsFooter" class="mb-6 w-fit">
+    <NuxtLink
+      :to="url"
+      target="_blank"
+      class="text-sm text-primary font-semibold"
+    >
+      <div class="flex items-center gap-2">
+        <SmartIcon :name="icon" />
+        <span>
+          {{ text }}
+        </span>
+      </div>
+    </NuxtLink>
+  </div>
+</template>
