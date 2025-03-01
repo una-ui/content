cab1eeb054d479ed1c85af671db21eb88accf81e
Phojie Rengel <phojrengel@gmail.com>
2025-03-01 19:39:44 +0800

    chore: fix lint

--- components/common/accordion/AccordionTrigger.vue
+++ components/common/accordion/AccordionTrigger.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
+import type { AccordionTriggerProps } from 'radix-vue'
+import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
 import {
   AccordionHeader,
   AccordionTrigger,
-  type AccordionTriggerProps,
+
 } from 'radix-vue'
-import { computed, type HTMLAttributes } from 'vue'
+import { computed } from 'vue'
 
 const props = defineProps<AccordionTriggerProps & { class?: HTMLAttributes['class'] }>()
 
--- components/common/toast/ToastClose.vue
+++ components/common/toast/ToastClose.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import type { ToastCloseProps } from 'radix-vue'
+import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
-import { ToastClose, type ToastCloseProps } from 'radix-vue'
-import { computed, type HTMLAttributes } from 'vue'
+import { ToastClose } from 'radix-vue'
+import { computed } from 'vue'
 
 const props = defineProps<ToastCloseProps & {
   class?: HTMLAttributes['class']
