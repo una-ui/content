11397c38faed30aed5d74118a77e0fc80fcdb2a4
Phojie Rengel <phojrengel@gmail.com>
2025-04-26 22:08:26 +0800

    style: adjust navigation menu trigger height to 8px

--- components/common/navigation-menu/index.ts
+++ components/common/navigation-menu/index.ts
@@ -9,5 +9,5 @@ export { default as NavigationMenuTrigger } from './NavigationMenuTrigger.vue'
 
 // @unocss-include
 export const navigationMenuTriggerStyle = cva(
-  'group inline-flex h-10 w-max items-center justify-center rounded-md bg-base px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent focus:bg-accent focus:text-accent focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-$c-accent/50 data-[state=open]:bg-$c-accent/50',
+  'group inline-flex h-8 w-max items-center justify-center rounded-md bg-base px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent focus:bg-accent focus:text-accent focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-$c-accent/50 data-[state=open]:bg-$c-accent/50',
 )
